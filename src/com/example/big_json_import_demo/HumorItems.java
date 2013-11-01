0f14cbe9bab943f02cb43cf5a190ad8f7c410d1b
Newbilius <newbilius@gmail.com>
2013-11-01 21:42:08 +0600

    Простое скачивание больших и очень больших файлов в JSON.

---
+++ src/com/example/big_json_import_demo/HumorItems.java
@@ -0,0 +1,7 @@
+package com.example.big_json_import_demo;
+
+import java.util.List;
+
+public class HumorItems {
+    List<HumorItem> Items;
+}
--- src/com/example/big_json_import_demo/Main.java
+++ src/com/example/big_json_import_demo/Main.java
@@ -6,25 +6,25 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class Main extends Activity {
-    String UrlSmall="http://siteszone.ru//test_data/small.json";
-    String UrlBig="http://siteszone.ru//test_data/big.json";
+    String UrlSmall="http://siteszone.ru/test_data/small.json";//7 килобайт
+    String UrlBig="http://siteszone.ru/test_data/big.json";    //5.79 мб.
+    String UrlVeryBig="http://siteszone.ru/test_data/very_big.json";    //13.3 мб.
 
     public static GsonBuilder gsonBuilder = new GsonBuilder();
     public static com.google.gson.Gson Gson = gsonBuilder.create();
+    public File cache_dir;
 
     public class LoadData extends AsyncTask<Void, Void, Void> {
 
@@ -39,7 +39,6 @@ public class Main extends Activity {
             //скачивание данных
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httppost = new HttpPost(_url);
-            String responseString=null;
 
             HttpResponse response = null;
             try {
@@ -59,16 +58,84 @@ public class Main extends Activity {
                 }
 
                 //парсинг данных
-                ArrayList<HumorItem> items;
-                Type listOfTestObjectsType = new TypeToken<ArrayList<HumorItem>>(){}.getType();
-                items= Gson.fromJson(responseBuilder.toString(),listOfTestObjectsType);
+                HumorItems list= Gson.fromJson(responseBuilder.toString(),HumorItems.class);
 
                 //тестовый вывод
-                for (HumorItem item:items){
-                    Log.d("Текст: "+item.text);
-                    Log.d("Ссылка: "+item.url);
+                for (HumorItem message:list.Items){
+                    Log.d("Текст: "+message.text);
+                    Log.d("Ссылка: "+message.url);
                     Log.d("-------------------");
                 }
+
+                Log.d("ВСЕГО СКАЧАНО "+list.Items.size());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("ошибка "+e.getMessage());
+            }
+
+            return null;
+        }
+    }
+
+    public class LoadBigData extends AsyncTask<Void, Void, Void> {
+
+        String _url="";
+
+        public LoadBigData(String url){
+            _url=url;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //скачивание данных
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(_url);
+
+            HttpResponse response = null;
+            try {
+                //скачивание данных
+                response = httpclient.execute(httppost);
+
+                HttpEntity httpEntity=response.getEntity();
+                InputStream stream = AndroidHttpClient.getUngzippedContent(httpEntity);
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                File file = new File(cache_dir, "temp_json_new.json");
+
+                if (file.exists()){
+                    file.delete();
+                }
+
+                file.createNewFile();
+
+                FileOutputStream fileOutputStream=new FileOutputStream(file,true);
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+
+
+                char[] buff = new char[1024*1024];
+                int read;
+                long FullSize=0;
+                while((read = bufferedReader.read(buff)) != -1) {
+                    bufferedWriter.write(buff,0,read);
+                    FullSize+=read;
+                    Log.d("скачано " + PrepareSize(FullSize));
+                }
+                bufferedWriter.flush();
+                fileOutputStream.close();
+
+
+                FileInputStream fileInputStream=new FileInputStream(file);
+                InputStreamReader reader = new InputStreamReader(fileInputStream);
+                HumorItems list= Gson.fromJson(reader,HumorItems.class);
+
+                    //тестовый вывод
+                    for (HumorItem message:list.Items){
+                        Log.d("Текст: "+message.text);
+                        Log.d("Ссылка: "+message.url);
+                        Log.d("-------------------");
+                    }
+                Log.d("ВСЕГО СКАЧАНО "+list.Items.size());
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -84,10 +151,19 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        cache_dir = this.getExternalCacheDir();
+
         //успешно скачивает
         //new LoadData(UrlSmall).execute();
+
         //падает, слишком большой
-        new LoadData(UrlBig).execute();
+        //new LoadData(UrlBig).execute();
+
+        //нормально качает большой
+        //new LoadBigData(UrlBig).execute();
+
+        //нормально качает очень большой
+        new LoadBigData(UrlVeryBig).execute();
     }
 
     public String PrepareSize(long size){
